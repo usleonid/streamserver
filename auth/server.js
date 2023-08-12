62da4292b2d59931376d597f71fcddbc92e15e2a
leonidu <leonidu@cando.co.il>
2023-08-12 18:46:26 +0300

    return streamkey

--- auth/server.js
+++ auth/server.js
@@ -8,7 +8,7 @@ app.post("/auth", function (req, res) {
   const streamkey = req.body.key;
 
   /* You can make a database of users instead :) */
-  if (streamkey === "") {
+  if (streamkey === "supersecret") {
     res.status(200).send();
     return;
   }
