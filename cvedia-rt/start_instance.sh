9c823dcb9c580ed0490b212a27271f2b8b8a8379
leonidu <leonidu@cando.co.il>
2023-08-14 15:18:09 +0300

    cvedia installed

---
+++ cvedia-rt/start_instance.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# launch CVEDIA-RT
+cvediart --remote 
+# ./run.sh --headless &
+
+# waiting for a CVEDIA-RT
+# sleep 10
+
+# # add instance throw the REST API
+# curl -X POST -H "Content-Type: application/json" \
+#   -d @/cvedia-rt/testing.json \
+#   http://localhost:8080/api/instance/add
+
+# # waiting for the ending of the process
+# sleep 5
